d2fac65f07bd8c2f499b14ab70f66d55d61df2c1
Michelle Parisi <parimm@farmingdale.edu>
2024-08-29 15:54:34 -0400

    Create the Car Class

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,9 @@
+package org.example;
+/*
+Car class, this class inheritance/extends from the Vehicle class.
+this class add the attribute: brand (String)
+ */
+public class Car extends Vehicle {
+    String brand;
+
+}
