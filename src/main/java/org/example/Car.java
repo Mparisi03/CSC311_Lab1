5fb975cf5c98b3c4bbc7b2c09f2af9698d702061
Michelle Parisi <parimm@farmingdale.edu>
2024-08-29 16:16:56 -0400

    Add Additional Methods in Car (plus Comments)

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -6,4 +6,57 @@ this class add the attribute: brand (String)
 public class Car extends Vehicle {
     String brand;
 
+    /**
+     * empty constructor(public)
+     */
+
+    public Car() {
+        super();
+    }
+
+    /**
+     * copy constructor(public)
+     * @param brand
+     */
+    public Car(String brand) {
+        super();
+        this.brand = brand;
+    }
+
+    /**
+     * setter for brand
+     * @param brand
+     */
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    /**
+     * Getter for brand
+     * @return
+     */
+    public String getBrand() {
+        return brand;
+    }
+
+    /**
+     * method that prints out Honk, honk!
+     */
+    public void honk(){
+        System.out.println("Honk, honk!");
+    }
+
+    /**
+     * method that prints out all the attributes inculding the one in Vehicle class
+     */
+    public void displayInfo(){
+        System.out.println("Brand: " + brand);
+        System.out.println("Number of wheels" + getNumberOfWheels());
+        System.out.println("Color: " + getColor());
+        System.out.println("Engine size" + getEngineSize());
+        System.out.println("Fuel type " + getFuelType());
+
+    }
+
 }
